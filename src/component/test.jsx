1de60ba76c0ef6f92439f3b034a9cb5cb13ad1b8
lee yoon jin <mngvmn0707@gmail.com>
2023-08-19 02:07:27 +0900

    first commit

---
+++ src/component/test.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+  render() {
+    const status = 'Next plater : X';
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
